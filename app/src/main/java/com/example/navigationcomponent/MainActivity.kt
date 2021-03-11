fbc3618012f7895554632ba4c7bb50ce60b9463d
Younis <eslamyounis34@gmail.com>
2021-03-11 22:32:40 +0200

    Add drawer

--- app/src/main/java/com/example/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/MainActivity.kt
@@ -5,58 +5,67 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-    private lateinit var  toolbar:Toolbar
+    private lateinit var toolbar: Toolbar
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navigationView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController=navHostFragment.findNavController()
-        toolbar=findViewById(R.id.custom_toolbar)
-        bottomNavigationView=findViewById(R.id.bottom_nav)
-        appBarConfiguration= AppBarConfiguration(
-            setOf(R.id.homeFragment,R.id.searchFragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        toolbar = findViewById(R.id.custom_toolbar)
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+        navigationView = findViewById(R.id.nav_view)
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+
+        //Add appbar to fragments and enable drawer in it
+        appBarConfiguration = AppBarConfiguration(
+                setOf(R.id.homeFragment, R.id.searchFragment),
+                drawerLayout
         )
 
+        //Add navigation graph to navigation views
         bottomNavigationView.setupWithNavController(navController)
+        navigationView.setupWithNavController(navController)
 
         setSupportActionBar(toolbar)
-        setupActionBarWithNavController(navController,appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
-        menuInflater.inflate(R.menu.options_menu,menu)
+        menuInflater.inflate(R.menu.options_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-      return  if (item.itemId==R.id.termsAndconditions) {
-          val action=NavGraphDirections.actionGlobalTermsFragment()
-          navController.navigate(action)
-          true
-      }else{
-          item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        return if (item.itemId == R.id.termsAndconditions) {
+            val action = NavGraphDirections.actionGlobalTermsFragment()
+            navController.navigate(action)
+            true
+        } else {
+            item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
 
-      }
+        }
     }
 }
